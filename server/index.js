02c3327b3f08bd82e57f90304e37d5ab885ef64f
Vasanth <vasanth.3656@yahoo.com>
2023-05-02 23:56:20 +0530

    first commit

--- server/index.js
+++ server/index.js
@@ -30,7 +30,7 @@ app.use("/product", productRoutes)
 app.use("/transaction", transactionRoutes)
 
 /* MONGOOSE SETUP */
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 9000
 
 console.log("Contecting.....")
 
